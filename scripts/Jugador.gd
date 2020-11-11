ec96dff3847f0abba6568e5a2dc9865efebb5406
Darío <dariodallara15@gmail.com>
2020-11-11 16:54:39 -0300

    aaa

--- scripts/Jugador.gd
+++ scripts/Jugador.gd
@@ -8,26 +8,26 @@ var velocity = Vector2()
 var jumping = false
 
 func get_input():
-    velocity.x = 0
-    var right = Input.is_action_pressed('right')
-    var left = Input.is_action_pressed('left')
-    var jump = Input.is_action_just_pressed('jump')
+	velocity.x = 0
+	var right = Input.is_action_pressed('right')
+	var left = Input.is_action_pressed('left')
+	var jump = Input.is_action_just_pressed('jump')
 
-    if jump and is_on_floor():
-        jumping = true
-        velocity.y = jump_speed
-    if right:
-        velocity.x += run_speed
-    if left:
-        velocity.x -= run_speed
+	if jump and is_on_floor():
+		jumping = true
+		velocity.y = jump_speed
+	if right:
+		velocity.x += run_speed
+	if left:
+		velocity.x -= run_speed
 
 func _ready():
 	#var jugador_vars = get_node("/root/Global")
 	pass
 
 func _physics_process(delta):
-    get_input()
-    velocity.y += gravity * delta
-    if jumping and is_on_floor():
-        jumping = false
-    velocity = move_and_slide(velocity, Vector2(0, -1))
+	get_input()
+	velocity.y += gravity * delta
+	if jumping and is_on_floor():
+		jumping = false
+	velocity = move_and_slide(velocity, Vector2(0, -1))
